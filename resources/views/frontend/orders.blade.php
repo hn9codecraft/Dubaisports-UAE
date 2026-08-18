7fcf9eb154fe5a89cbf34408540dbb3d70862a60
Developer <developer@dubaisportsuae.com>
2026-08-18 23:51:40 +0530

    feat: add beautiful empty state design for my orders page

--- resources/views/frontend/orders.blade.php
+++ resources/views/frontend/orders.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             @include('frontend.layouts.customer_sidebar')
             <div class="col-md-9">
-            @if($orders)
+            @if(count($orders) > 0)
                 @foreach($orders as $key => $order)
                 @php
                     $products = json_decode($order['products'], true);
@@ -85,6 +85,14 @@
                 </div>
                 @endforeach
             @else
+                <div class="text-center py-5 bg-white border border-secondary shadow-sm rounded">
+                    <div class="mb-4 mt-3">
+                        <i class="fa-solid fa-box-open text-muted opacity-50" style="font-size: 5rem;"></i>
+                    </div>
+                    <h4 class="text-dark fw-bold mb-2">No orders here yet!</h4>
+                    <p class="text-muted mb-4">Looks like you haven't made your first purchase yet.</p>
+                    <a href="{{ route('home') }}" class="btn btn-primary px-4 py-2 fw-bold mb-3">Start Shopping</a>
+                </div>
             @endif
             </div>
         </div>
